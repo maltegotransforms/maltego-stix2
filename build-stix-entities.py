9d69fdce37b5bd058b610d33b4bbf12412d904dc
Samuel Hassine <samuel@hassine.fr>
2020-03-15 01:58:01 +0100

    Black codestyle

--- build-stix-entities.py
+++ build-stix-entities.py
@@ -2,100 +2,138 @@ import os
 import json
 from config import schema_config, heritage_config
 
+
 def resolve_refs(schema, path):
-	if isinstance(schema, dict):
-		if "$ref" in schema and not schema["$ref"].startswith("#"):
-			with open(os.path.join(path, schema["$ref"]), "r") as ref_file:
-				ref_schema = json.load(ref_file)
-				for key, value in schema.items():
-					if key != "$ref":
-						ref_schema[key] = value
-				return resolve_refs(ref_schema, os.path.dirname(os.path.join(path, schema["$ref"])))
-		else:
-			new_dict = {}
-			for key, value in schema.items():
-				new_dict[key] = resolve_refs(value, path)
-			return new_dict
-	elif isinstance(schema, list):
-		new_list = []
-		for l in schema:
-			new_list.append(resolve_refs(l, path))
-		return new_list
-	else:
-		return schema
+    if isinstance(schema, dict):
+        if "$ref" in schema and not schema["$ref"].startswith("#"):
+            with open(os.path.join(path, schema["$ref"]), "r") as ref_file:
+                ref_schema = json.load(ref_file)
+                for key, value in schema.items():
+                    if key != "$ref":
+                        ref_schema[key] = value
+                return resolve_refs(
+                    ref_schema, os.path.dirname(os.path.join(path, schema["$ref"]))
+                )
+        else:
+            new_dict = {}
+            for key, value in schema.items():
+                new_dict[key] = resolve_refs(value, path)
+            return new_dict
+    elif isinstance(schema, list):
+        new_list = []
+        for l in schema:
+            new_list.append(resolve_refs(l, path))
+        return new_list
+    else:
+        return schema
+
 
 categories = []
 
 # Read schemas
 for schema in schema_config:
-	for entity_file_name in os.listdir(schema["path"]):
-		with open(os.path.join(schema["path"], entity_file_name), "r") as entity_file:
-			entity_schema = json.load(entity_file)
-			entity_schema = resolve_refs(entity_schema, schema["path"])
+    for entity_file_name in os.listdir(schema["path"]):
+        with open(os.path.join(schema["path"], entity_file_name), "r") as entity_file:
+            entity_schema = json.load(entity_file)
+            entity_schema = resolve_refs(entity_schema, schema["path"])
 
-			fields = {}
-			# Only the last occurrence of each proprety is kept to handle inheritance
-			for properties in entity_schema["allOf"]:
-				if "properties" in properties:
-					for key, value in properties["properties"].items():
-						property_type = "string"
-						if "type" in value:
-							if value["type"] == "array":
-								property_type = "string[]"
-						if "enum" in value and len(value["enum"]) == 1:
-							data = {
-								"name": key,
-								"type": property_type,
-								"description": value["description"] if "description" in value else "",
-								"value": value["enum"][0]
-							}
-							fields[key] = '         <Field name="{name}" type="{type}" nullable="true" hidden="false" readonly="true" description="{description}">\n            <DefaultValue>{value}</DefaultValue>\n            <SampleValue>{value}</SampleValue>\n         </Field>\n'.format(**data)
-						else:
-							data = {
-								"name": key,
-								"type": property_type,
-								"description": value["description"] if "description" in value else ""
-							}
-							fields[key] = '         <Field name="{name}" type="{type}" nullable="true" hidden="false" readonly="false" description="{description}"/>\n'.format(**data)
+            fields = {}
+            # Only the last occurrence of each proprety is kept to handle inheritance
+            for properties in entity_schema["allOf"]:
+                if "properties" in properties:
+                    for key, value in properties["properties"].items():
+                        property_type = "string"
+                        if "type" in value:
+                            if value["type"] == "array":
+                                property_type = "string[]"
+                        if "enum" in value and len(value["enum"]) == 1:
+                            data = {
+                                "name": key,
+                                "type": property_type,
+                                "description": value["description"]
+                                if "description" in value
+                                else "",
+                                "value": value["enum"][0],
+                            }
+                            fields[
+                                key
+                            ] = '         <Field name="{name}" type="{type}" nullable="true" hidden="false" readonly="true" description="{description}">\n            <DefaultValue>{value}</DefaultValue>\n            <SampleValue>{value}</SampleValue>\n         </Field>\n'.format(
+                                **data
+                            )
+                        else:
+                            data = {
+                                "name": key,
+                                "type": property_type,
+                                "description": value["description"]
+                                if "description" in value
+                                else "",
+                            }
+                            fields[
+                                key
+                            ] = '         <Field name="{name}" type="{type}" nullable="true" hidden="false" readonly="false" description="{description}"/>\n'.format(
+                                **data
+                            )
 
-			# Export entity
-			with open("./templates/template.entity", "r") as entity_template:
-				base_entity = ""
-				if entity_schema["title"] in heritage_config and heritage_config[entity_schema["title"]] != "":
-					base_entity = "      <BaseEntity>"+heritage_config[entity_schema["title"]]+"</BaseEntity>"
-				data = {
-					"id": "STIX2." + entity_schema["title"],
-					"displayName": entity_schema["title"].replace('-', ' ').title(),
-					"namePlural": entity_schema["title"].replace('-', ' ').title(),
-					"description": entity_schema["description"].strip(),
-					"category": schema["category"],
-					"smallIconResource": "stix2_" + entity_schema["title"].replace('-', '_'),
-					"largeIconResource": "stix2_" + entity_schema["title"].replace('-', '_'),
-					"mainValue": (
-						"name" if "name" in fields else (
-							"value" if "value" in fields else (
-								"relationship_type" if "relationship_type" in fields else
-									"id"
-							)
-						)
-					),
-					"fields": "".join(v for k,v in fields.items()),
-					"baseEntities": base_entity
-				}
-				t = entity_template.read()
-				with open("./mtz/Entities/"+data["id"]+".entity", "w") as output:
-					output.write(t.format(**data))
+            # Export entity
+            with open("./templates/template.entity", "r") as entity_template:
+                base_entity = ""
+                if (
+                    entity_schema["title"] in heritage_config
+                    and heritage_config[entity_schema["title"]] != ""
+                ):
+                    base_entity = (
+                        "      <BaseEntity>"
+                        + heritage_config[entity_schema["title"]]
+                        + "</BaseEntity>"
+                    )
+                data = {
+                    "id": "STIX2." + entity_schema["title"],
+                    "displayName": entity_schema["title"].replace("-", " ").title(),
+                    "namePlural": entity_schema["title"].replace("-", " ").title(),
+                    "description": entity_schema["description"].strip(),
+                    "category": schema["category"],
+                    "smallIconResource": "stix2_"
+                    + entity_schema["title"].replace("-", "_"),
+                    "largeIconResource": "stix2_"
+                    + entity_schema["title"].replace("-", "_"),
+                    "mainValue": (
+                        "name"
+                        if "name" in fields
+                        else (
+                            "value"
+                            if "value" in fields
+                            else (
+                                "relationship_type"
+                                if "relationship_type" in fields
+                                else "id"
+                            )
+                        )
+                    ),
+                    "fields": "".join(v for k, v in fields.items()),
+                    "baseEntities": base_entity,
+                }
+                t = entity_template.read()
+                with open("./mtz/Entities/" + data["id"] + ".entity", "w") as output:
+                    output.write(t.format(**data))
 
-				print(entity_schema["title"].replace('-', ' ').title().replace(' ', '') + ' = "' + data["id"] + '"')
+                print(
+                    entity_schema["title"].replace("-", " ").title().replace(" ", "")
+                    + ' = "'
+                    + data["id"]
+                    + '"'
+                )
 
-			# Export category if new
-			if schema["category"] not in categories:
-				with open("./templates/template.category", "r") as entity_template:
-					data = {
-						"name": schema["category"]
-					}
-					t = entity_template.read()
-					with open("./mtz/EntityCategories/"+schema["category"].lower().replace(" ","-")+".category", "w") as output:
-						output.write(t.format(**data))
+            # Export category if new
+            if schema["category"] not in categories:
+                with open("./templates/template.category", "r") as entity_template:
+                    data = {"name": schema["category"]}
+                    t = entity_template.read()
+                    with open(
+                        "./mtz/EntityCategories/"
+                        + schema["category"].lower().replace(" ", "-")
+                        + ".category",
+                        "w",
+                    ) as output:
+                        output.write(t.format(**data))
 
-					categories.append(schema["category"])
+                    categories.append(schema["category"])
--- config.py
+++ config.py
@@ -1,57 +1,57 @@
 # Config of JSON-REF STIX 2 Schemas to process
 # Each element should follow the format :
 # {
-#	 "path": string, # path to a folder containing a JSON REF schema
-#	 "category": string   # Maltego Entity Category to group entities generated from this schema
+# 	 "path": string, # path to a folder containing a JSON REF schema
+# 	 "category": string   # Maltego Entity Category to group entities generated from this schema
 # }
-# 
+#
 schema_config = [
-	{
-		"path": "./cti-stix2-json-schemas/schemas/sdos/",
-		"category": "STIX 2 domain objects"
-	},
-	{
-		"path": "./cti-stix2-json-schemas/schemas/sros/",
-		"category": "STIX 2 relationship objects"
-	},
-	{
-		"path": "./cti-stix2-json-schemas/schemas/observables/",
-		"category": "STIX 2 observables"
-	},
-	{
-		"path": "./cti-stix2-json-schemas-extended/schemas/sdos/",
-		"category": "STIX 2 domain objects"	
-	}
+    {
+        "path": "./cti-stix2-json-schemas/schemas/sdos/",
+        "category": "STIX 2 domain objects",
+    },
+    {
+        "path": "./cti-stix2-json-schemas/schemas/sros/",
+        "category": "STIX 2 relationship objects",
+    },
+    {
+        "path": "./cti-stix2-json-schemas/schemas/observables/",
+        "category": "STIX 2 observables",
+    },
+    {
+        "path": "./cti-stix2-json-schemas-extended/schemas/sdos/",
+        "category": "STIX 2 domain objects",
+    },
 ]
 
 # Config to handle Maltego entities inheritance
 # Format :
 # "stix_type": "parent_maltego_entity_id"
 heritage_config = {
-	"autonomous-system": "maltego.AS",
-	"domain-name": "maltego.Domain",
-	"directory": "maltego.custom.entities.indicators.systemindicator",
-	"file": "maltego.custom.entities.AnyFile",
-	"email-addr": "maltego.EmailAddress",
-	"email-message": "maltego.Document",
-	"identity": "maltego.Organization",
-	"incident": "maltego.Incident",
-	"indicator": "maltego.String",
-	"infrastructure": "maltego.custom.entities.indicators.networkindicator",
-	"intrusion-set": "cyrildemonceaux.Attacker",
-	"ipv4-addr": "maltego.IPv4Address",
-	"ipv6-addr": "maltego.custom.entities.infrastructure.IPv6Address",
-	"location": "maltego.Location",
-	"mac-addr": "maltego.custom.entities.indicators.networkindicator",
-	"malware": "maltego.custom.entities.malwares.MalwareFamily",
-	"mutex": "maltego.custom.entities.configuration.Mutex",
-	"process": "maltego.custom.entities.indicators.systemindicator",
-	"threat-actor": "cyrildemonceaux.Attacker",
-	"url": "maltego.URL",
-	"user-account": "maltego.custom.entities.indicators.systemindicator",
-	"vulnerability": "maltego.Exploit",
-	"windows-registry-key": "maltego.custom.entities.indicators.systemindicator",
-	"x509-certificate": "maltego.custom.entities.certificates.Certificatex509"
+    "autonomous-system": "maltego.AS",
+    "domain-name": "maltego.Domain",
+    "directory": "maltego.custom.entities.indicators.systemindicator",
+    "file": "maltego.custom.entities.AnyFile",
+    "email-addr": "maltego.EmailAddress",
+    "email-message": "maltego.Document",
+    "identity": "maltego.Organization",
+    "incident": "maltego.Incident",
+    "indicator": "maltego.String",
+    "infrastructure": "maltego.custom.entities.indicators.networkindicator",
+    "intrusion-set": "cyrildemonceaux.Attacker",
+    "ipv4-addr": "maltego.IPv4Address",
+    "ipv6-addr": "maltego.custom.entities.infrastructure.IPv6Address",
+    "location": "maltego.Location",
+    "mac-addr": "maltego.custom.entities.indicators.networkindicator",
+    "malware": "maltego.custom.entities.malwares.MalwareFamily",
+    "mutex": "maltego.custom.entities.configuration.Mutex",
+    "process": "maltego.custom.entities.indicators.systemindicator",
+    "threat-actor": "cyrildemonceaux.Attacker",
+    "url": "maltego.URL",
+    "user-account": "maltego.custom.entities.indicators.systemindicator",
+    "vulnerability": "maltego.Exploit",
+    "windows-registry-key": "maltego.custom.entities.indicators.systemindicator",
+    "x509-certificate": "maltego.custom.entities.certificates.Certificatex509",
 }
 
 # Config of PNG files to process to create Maltego icons
@@ -61,16 +61,13 @@ heritage_config = {
 # 	"filter": string   # part of the PNG files names to proces
 # 	"replace": {"string_to_replace": "replacement"} # Search and replace in PNG file names
 # }
-# The icon name should shart with the type of the entity (eg "malware") 
+# The icon name should shart with the type of the entity (eg "malware")
 # If several icons are available for each entity, the filter parameter can be used to filter
 # on the ones to keep. The icons names shoud contain this string just after the entity type.
 icons_config = [
-	{
-		"path": "./stix2-graphics/icons/png/",
-		"filter": "-round-flat-300",
-		"replace": {
-			"coa": "course_of_action",
-			"http": "url"
-		}
-	}
+    {
+        "path": "./stix2-graphics/icons/png/",
+        "filter": "-round-flat-300",
+        "replace": {"coa": "course_of_action", "http": "url"},
+    }
 ]
